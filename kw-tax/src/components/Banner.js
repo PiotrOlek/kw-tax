6164d48a10400110561c967332ce688408195a47
piotr.olek <olo_piotr@onet.pl>
2023-07-06 00:28:57 +0200

    update

---
+++ kw-tax/src/components/Banner.js
@@ -0,0 +1,96 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from "react";
+// images
+import Image from "../assets/avatar.png";
+// type animation
+import { TypeAnimation } from "react-type-animation";
+// motion
+import { motion } from "framer-motion";
+// variants
+import { fadeIn } from "../variants";
+
+const Banner = () => {
+  return (
+    <section
+      className="min-h-[100vh] lg:min-h-[65vh] flex items-center"
+      id="home"
+    >
+      <div className="container mx-auto">
+        <div className="flex flex-col gap-y-8 lg:flex-row lg:items-center">
+          {/* text */}
+          <div className="flex-1 text-center font-secondary lg:text-left">
+            <motion.h1
+              variants={fadeIn("up", 0.3)}
+              initial="hidden"
+              whileInView={"show"}
+              viewport={{ once: false, amount: 0.7 }}
+              className="text-[42px] font-bold leading-[0.8] lg:text-[80px]"
+            >
+              
+            </motion.h1>
+            <motion.div
+             variants={fadeIn("up", 0.4)}
+             initial="hidden"
+             whileInView={"show"}
+             viewport={{ once: false, amount: 0.7 }}
+             className="mb-6 text-[32px] lg:text-[65px] font-secondary font-semibold uppercase leading-[1]">
+              <span className="mr-4 text-white">Biuro</span>
+              <TypeAnimation
+                sequence={[
+                  "Księgowe",
+                  3000,
+                  "Rachunkowe",
+                  3000,
+                ]}
+                speed={10}
+                className="bg-gradient-to-r text-transparent bg-clip-text from-[#fdd4bb] to-[#c9a690] font-bold font-primary;"
+                wrapper="span"
+                repeat={Infinity}
+              />
+            </motion.div>
+            <motion.p 
+             variants={fadeIn("up", 0.5)}
+             initial="hidden"
+             whileInView={"show"}
+             viewport={{ once: false, amount: 0.7 }}
+            className="mb-8 max-w-lg mx-auto lg:mx-0">
+              KW-Tax to nowoczesne i przyjazne klientowi biuro rachunkowe z obsługą online, działające w powiecie Wałbrzych. Specjalizujemy się w zakładaniu działalności gospodarczej oraz spółek, a także kompleksowej obsłudze księgowej, rachunkowej, kadrowo-płacowej dla mikro, małych i średnich firm, a także jednoosobowych działalności gospodarczych. Zapraszamy do skorzystania z naszych usług.
+            </motion.p>
+            <motion.div 
+             variants={fadeIn("up", 0.6)}
+             initial="hidden"
+             whileInView={"show"}
+             viewport={{ once: false, amount: 0.7 }}
+            className="flex max-w-max gap-x-6 items-center mb-12 mx-auto lg:mx-0">
+              <button className="btn btn-lg">Cennik</button>
+              <a href="#" className="text-gradient btn-link">
+                Kontakt
+              </a>
+            </motion.div>
+            {/* social links */}
+            <motion.div 
+             variants={fadeIn("up", 0.7)}
+             initial="hidden"
+             whileInView={"show"}
+             viewport={{ once: false, amount: 0.7 }}
+            className="flex text-[20px] gap-x-6 max-w-max mx-auto lg:mx-0">
+             
+            </motion.div>
+          </div>
+          {/* images */}
+          <motion.div 
+           variants={fadeIn("down", 0.5)}
+           initial="hidden"
+           whileInView={"show"}
+           
+          className="hidden lg:flex flex-1 max-w-[1000px] lg:max-w-[1000px] w-80 ">
+            <img src={Image} alt="" className="ml-2"/>
+          </motion.div>
+        </div>
+      </div>
+      
+    </section>
+  );
+};
+
+export default Banner;
---
+++ kw-tax/src/components/Header.js
@@ -0,0 +1,22 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from 'react';
+// import logo
+import Logo from '../assets/Logo_KWTax.png';
+
+const Header = () => {
+  return <header className='py-8'>
+    <div className='container mx-auto'>
+      <div className='flex justify-between items-center'>
+        {/* logo */}
+        <a href='#'>
+          <img src={Logo} alt='' style={{width: '200px', height: '130px'}}/>
+        </a>
+
+        {/* button */}
+        <button className='btn btn-sm'>Zobacz oferte</button>
+      </div>
+    </div>
+  </header>;
+};
+
+export default Header;
